bebc85dba898d886cb5cd5f7ba0393bdac9a5ea1
ken862734801 <kenshawmichaud@gmail.com>
2023-05-16 15:40:29 -0400

    add printNumberSwap and printNumberSwap2 function

--- functions/script.js
+++ functions/script.js
@@ -1,7 +1,26 @@
-function printReverseOrder(){
-    for(let i = 50; i >= 0; i--){
+
+function printReverseOrder(num){
+    for(let i = num; i >= 0; i--){
         console.log(i)
     }
 };
 
-printReverseOrder();
+printReverseOrder(50);
+
+function printNumberSwap(a, b){
+    console.log("1:" + a, "2:"+ b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    console.log("1:" + a, "2:" + b)
+}
+
+printNumberSwap(5, 10);
+
+function printNumberSwap2(a, b){
+    console.log("1:"+ a, "2:"+ b);
+    [a, b] = [b, a];
+    console.log("1:"+ a, "2:" + b);
+}
+
+printNumberSwap2(5, 10)
