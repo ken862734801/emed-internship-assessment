f16fd186e0856cec18d97bc376756dc616f29e96
ken862734801 <kenshawmichaud@gmail.com>
2023-05-16 16:18:51 -0400

    add printFactorial function

--- functions/script.js
+++ functions/script.js
@@ -1,3 +1,5 @@
+// This function prints numbers in reverse order starting from the given number.
+// Parameter: num - The number from which the reverse order starts.
 
 function printReverseOrder(num){
     for(let i = num; i >= 0; i--){
@@ -7,6 +9,24 @@ function printReverseOrder(num){
 
 printReverseOrder(50);
 
+// This function prints the factorial of the given number.
+// Parameters: num - The number from which we find the factorial.
+function printFactorial(num){
+    let ans = 1;
+    if (num === 0 || num === 1){
+        console.log(1);
+    }
+    for(let i = 2; i <= num; i++){
+        ans *= i;
+    }
+    console.log(ans);
+};
+
+printFactorial(5);
+
+// This function swaps the values of two variables using arithmetic operations.
+// Parameters: a - The first number to swap.
+//             b - The second number to swap.
 function printNumberSwap(a, b){
     console.log("1:" + a, "2:"+ b);
     a = a + b;
@@ -17,10 +37,13 @@ function printNumberSwap(a, b){
 
 printNumberSwap(5, 10);
 
+// This function swaps the values of two variables using destructuring assignment.
+// Parameters: a - The first number to swap.
+//             b - The second number to swap.
 function printNumberSwap2(a, b){
     console.log("1:"+ a, "2:"+ b);
     [a, b] = [b, a];
     console.log("1:"+ a, "2:" + b);
 }
 
-printNumberSwap2(5, 10)
+printNumberSwap2(5, 10);
