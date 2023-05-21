ed5f04561da761e8142fea933f1c12cd9d50d5c3
ken862734801 <kenshawmichaud@gmail.com>
2023-05-21 13:06:37 -0400

    add additional test to cart and checkout step two

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -4,11 +4,17 @@ class CartPage extends Page {
     get cartItem (){
         return $('div[class="cart_item"]')
     }
+    get removeSauceLabsBackpackButton(){
+        return $('button[data-test="remove-sauce-labs-backpack"]')
+    }
     get continueShoppingButton(){
         return $('button[data-test="continue-shopping"]')
     }
     get checkoutButton(){
         return $('button[data-test="checkout"]')
+    }
+    async remove(){
+        await this.removeSauceLabsBackpackButton.click();
     }
     async continue(){
         await this.continueShoppingButton.click();
--- test/pageobjects/checkout.step.two.page.js
+++ test/pageobjects/checkout.step.two.page.js
@@ -4,6 +4,18 @@ import Page from "./page.js";
 
 class CheckoutStepTwoPage extends Page {
 
+    get cartItemName(){
+        return $('div[class="inventory_item_name"]')
+    }
+    get cartItemDescription(){
+        return $('div[class="inventory_item_desc"]')
+    }
+    get cartItemPrice(){
+        return $('div[class="inventory_item_price"]')
+    }
+    get summaryInformation(){
+        return $('div[class="summary_info"]')
+    }
     get cancelButton(){
         return $('button[data-test="cancel"]')
     }
